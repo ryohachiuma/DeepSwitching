6aa5972476d617f00cd9e50fc91718a4215e586a
ryo <ryo-hachiuma@hvrl.ics.keio.ac.jp>
2020-02-07 09:25:33 +0900

    first commit

---
+++ switching/train.py
@@ -0,0 +1,121 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import argparse
+import os
+import sys
+import pickle
+import time
+import torch
+import numpy as np
+sys.path.append(os.getcwd())
+
+from utils import *
+from models.DSNet import *
+from switching.utils.DSNet_dataset import Dataset
+from switching.utils.DSNet_config import Config
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--cfg', default=None)
+parser.add_argument('--mode', default='train')
+parser.add_argument('--data', default=None)
+parser.add_argument('--gpu-index', type=int, default=0)
+parser.add_argument('--iter', type=int, default=0)
+args = parser.parse_args()
+if args.data is None:
+    args.data = args.mode if args.mode in {'train', 'test'} else 'train'
+
+cfg = Config(args.action, args.cfg, create_dirs=(args.iter == 0))
+
+"""setup"""
+dtype = torch.float64
+torch.set_default_dtype(dtype)
+device = torch.device('cuda', index=args.gpu_index) if torch.cuda.is_available() else torch.device('cpu')
+if torch.cuda.is_available():
+    torch.cuda.set_device(args.gpu_index)
+np.random.seed(cfg.seed)
+torch.manual_seed(cfg.seed)
+tb_logger = Logger(cfg.tb_dir)
+logger = create_logger(os.path.join(cfg.log_dir, 'log.txt'))
+
+dataset = Dataset(cfg, cfg.meta_id, args.data, cfg.fr_num, cfg.iter_method, cfg.shuffle, 2*cfg.fr_margin, cfg.num_sample)
+
+"""networks"""
+dsnet = DSNet(2, cfg.v_hdim, cfg.cnn_fdim, \
+                        mlp_dim=cfg.mlp_dim, v_net_param=cfg.v_net_param, \
+                            bi_dir=cfg.bi_dir, training=(args.mode == 'train'), is_dropout=cfg.is_dropout)
+
+
+if args.iter > 0:
+    cp_path = '%s/iter_%04d.p' % (cfg.model_dir, args.iter)
+    logger.info('loading model from checkpoint: %s' % cp_path)
+    model_cp, meta = pickle.load(open(cp_path, "rb"))
+    dsnet.load_state_dict(model_cp['state_net_dict'], strict=True)
+
+dsnet.to(device)
+
+ce_loss = nn.CrossEntropyLoss()
+
+if cfg.optimizer == 'Adam':
+    optimizer = torch.optim.Adam(dsnet.parameters(), lr=cfg.lr, weight_decay=cfg.weightdecay)
+else:
+    optimizer = torch.optim.SGD(dsnet.parameters(), lr=cfg.lr, weight_decay=cfg.weightdecay)
+fr_margin = cfg.fr_margin
+
+
+
+if args.mode == 'train':
+    dsnet.train()
+    for i_epoch in range(args.iter, cfg.num_epoch):
+        t0 = time.time()
+        epoch_num_sample = 0
+        epoch_loss = 0
+        """img: (B, Cam, S, H, W, Channel)
+           labels: (B, S, Cam)
+        """
+        for imgs_np, labels_np in dataset:
+            num = imgs_np.shape[2] - 2 * fr_margin
+            imgs = tensor(imgs_np, dtype=dtype, device=device)
+            labels = tensor(labels_np, dtype=dtype, device=device)
+            prob_pred, indices_pred = dsnet(imgs)
+
+            prob_pred = prob_pred[:, :, fr_margin: -fr_margin, :]
+            indices_pred = indices_pred[:, :, fr_margin:-fr_margin]
+
+            """Compute loss"""
+            """1. Categorical Loss"""
+            """TODO: Change from cross entropy to focal loss"""
+            cat_loss = ce_loss(prob_pred.view(-1, 2), labels.view(-1,))
+
+            """2. Switching loss: if the selected camera is different from the next selection, 
+            penalize that."""
+            prev_indices_pred = indices_pred[:, :-1]
+            next_indices_pred = indices_pred[:, 1: ]
+            diff_loss = torch.abs(next_indices_pred - prev_indices_pred)
+            diff_loss = torch.sum(diff_loss / (diff_loss + 1e-8), dim=1)
+
+            loss = cat_loss + cfg.w_d * diff_loss
+
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            # logging
+            epoch_loss += loss.cpu().item() * num
+            epoch_num_sample += num
+
+            """clean up gpu memory"""
+            torch.cuda.empty_cache()
+
+        epoch_loss /= epoch_num_sample
+        logger.info('epoch {:4d}    time {:.2f}     nsample {}   loss {:.4f} '
+                    .format(i_epoch, time.time() - t0, epoch_num_sample, epoch_loss))
+        tb_logger.scalar_summary('loss', epoch_loss, i_epoch)
+
+        with to_cpu(dsnet):
+            if cfg.save_model_interval > 0 and (i_epoch + 1) % cfg.save_model_interval == 0:
+                cp_path = '%s/iter_%04d.p' % (cfg.model_dir, i_epoch + 1)
+                model_cp = {'ds_net': dsnet.state_dict()}
+                pickle.dump(model_cp, open(cp_path, 'wb'))
---
+++ switching/utils/DSNet_config.py
@@ -0,0 +1,52 @@
+import yaml
+import os
+from utils import recreate_dirs
+
+
+class Config:
+
+    def __init__(self, action, cfg_id, create_dirs=False):
+        self.id = cfg_id
+        cfg_name = 'config/%s.yml' % cfg_id
+        if not os.path.exists(cfg_name):
+            print("Config file doesn't exist: %s" % cfg_name)
+            exit(0)
+        cfg = yaml.load(open(cfg_name, 'r'), Loader=yaml.FullLoader)
+
+        # create dirs
+        self.base_dir = 'results'
+        self.data_dir = cfg.get('dataset_path', 'datasets')
+        self.cfg_dir = '%s/%s' % (self.base_dir, cfg_id)
+        self.model_dir = '%s/models' % self.cfg_dir
+        self.result_dir = '%s/results' % self.cfg_dir
+        self.log_dir = '%s/log' % self.cfg_dir
+        self.tb_dir = '%s/tb' % self.cfg_dir
+        os.makedirs(self.model_dir, exist_ok=True)
+        os.makedirs(self.result_dir, exist_ok=True)
+        if create_dirs:
+            recreate_dirs(self.log_dir, self.tb_dir)
+
+        self.seed = cfg['seed']
+        self.fr_num = cfg['fr_num']
+        self.v_net = cfg.get('v_net', 'lstm')
+        self.v_net_param = cfg.get('v_net_param', None)
+        self.v_hdim = cfg['v_hdim']
+        self.mlp_dim = cfg['mlp_dim']
+        self.cnn_fdim = cfg['cnn_fdim']
+        self.optimizer = cfg.get('optimizer', 'Adam')
+        self.lr = cfg['lr']
+        self.num_epoch = cfg['num_epoch']
+        self.iter_method = cfg['iter_method']
+        self.shuffle = cfg.get('shuffle', False)
+        self.num_sample = cfg.get('num_sample', 20000)
+        self.save_model_interval = cfg['save_model_interval']
+        self.fr_margin = cfg['fr_margin']
+        self.causal = cfg.get('causal', False)
+        self.cnn_type = cfg.get('cnn_type', 'resnet')
+        self.is_dropout = cfg.get('dropout', False)
+        self.weightdecay = cfg.get('weight_decay', 0.0)
+        self.bi_dir = cfg.get('bi_dir', True)
+        self.augment = cfg.get('augment', False)
+
+        self.w_d = cfg.get('w_d', 1.0)
+
