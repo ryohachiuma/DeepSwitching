7f032fb7f939f87a73f4a9ee4b8a589f48d0552c
ryo <ryo-hachiuma@hvrl.ics.keio.ac.jp>
2020-02-10 10:43:51 +0900

    focal loss

--- models/DSNet.py
+++ models/DSNet.py
@@ -73,7 +73,7 @@ class DSNet(nn.Module):
         #batch, self.frame_num
         max_indices = max_indices.view(-1, self.frame_num)
 
-        return logits, max_indices
+        return torch.log(logits), max_indices
 
 
 if __name__ == '__main__':
