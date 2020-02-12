1a1bf3b8a192c28a97f4ee6bf235dd5a9dc18303
ryo <ryo-hachiuma@hvrl.ics.keio.ac.jp>
2020-02-12 14:56:38 +0900

    t

--- switching/train.py
+++ switching/train.py
@@ -145,6 +145,8 @@ elif args.mode == 'test':
     take = dataset.takes[0]
     #take_start_ind[take] = dataset.fr_lb + fr_margin
     for imgs_np, labels_np, _ in dataset:
+        if not take in take_start_ind:
+            take_start_ind[take] = dataset.fr_lb + fr_margin
         t0 = time.time()
         imgs = tensor(imgs_np, dtype=dtype, device=device)
         prob_pred, _ = dsnet(imgs)
