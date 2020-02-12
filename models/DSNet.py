7b200767d4a08debf99ab613b2454aa7e29d8502
ryo <ryo-hachiuma@hvrl.ics.keio.ac.jp>
2020-02-12 14:13:37 +0900

    test

--- models/DSNet.py
+++ models/DSNet.py
@@ -85,9 +85,7 @@ class DSNet(nn.Module):
         #batch x cameraNum, framenum, cnn_fdimx2 
         cam_features = torch.cat([local_feat, glob_feat], -1).view(-1, self.frame_num, self.cnn_fdim * 2)
         #batch x cameraNum x framenum, v_hdimx2
-        seq_features = self.v_net(cam_features)[0]
-        print(seq_features.size())
-        seq_features = seq_features.view(-1, self.v_hdim * 2)
+        seq_features = self.v_net(cam_features)[0].contiguous().view(-1, self.v_hdim * 2)
         #batch x cameraNum x framenum, mlp_dim[-1] 
         seq_features = self.mlp(seq_features)
         #batch, cameraNum, framenum, 2
