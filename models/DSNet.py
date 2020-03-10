b4e9425177ebc1ecc348ece9c7b5a27218355356
ryo <ryo-hachiuma@hvrl.ics.keio.ac.jp>
2020-03-10 14:44:29 +0900

    dsnet

--- models/DSNet.py
+++ models/DSNet.py
@@ -154,8 +154,54 @@ class DSNet(nn.Module):
         return torch.log(logits)
     
 
+
+class DSNetv2(nn.Module):
+
+    def __init__(self, out_dim, v_hdim, cnn_fdim, dtype, device, frame_num=10, camera_num=3, frame_shape=(3, 224, 224), mlp_dim=(128, 64),
+                 v_net_type='lstm', v_net_param=None, bi_dir=False, training=True, is_dropout=False):
+        super().__init__()
+        self.out_dim = out_dim
+        self.cnn_fdim = cnn_fdim
+        self.v_hdim = v_hdim
+        self.frame_shape = frame_shape
+        self.camera_num = camera_num
+        self.cnn = ResNet(cnn_fdim, running_stats=training)
+        self.dtype = dtype
+        self.device = device
+
+        self.v_net_type = v_net_type
+        #self.v_net = RNN(cnn_fdim * 2, v_hdim, bi_dir=bi_dir)
+        self.v_net = nn.LSTM(cnn_fdim * 2, v_hdim // 2, 2, batch_first=True, dropout=0.01, bidirectional=bi_dir)
+        self.mlp = MLP(v_hdim, mlp_dim, 'leaky', is_dropout=is_dropout)
+        self.linear = nn.Linear(self.mlp.out_dim, out_dim)
+        self.softmax = nn.Softmax(dim=1)
+
+
+    def forward(self, inputs):
+        fr_num = inputs.size()[2]
+        #batch x cameraNum, framenum, cnn_fdim
+        local_feat = self.cnn(inputs.view((-1,) + self.frame_shape)).view((-1, fr_num, self.cnn_fdim))
+        #batch, cameraNum, framenum, cnn_fdim
+        local_feat = local_feat.contiguous().view(-1, self.camera_num, fr_num, self.cnn_fdim)
+        #batch, 1, framenum, cnn_fdim
+        glob_feat = torch.max(local_feat, 1, keepdim=True)[0]
+        #batch, cameraNum, framenum, cnn_fdim
+        glob_feat = glob_feat.repeat(1, self.camera_num, 1, 1)
+        #framenum, batch x cameraNum, cnn_fdimx2 
+        cam_features = torch.cat([local_feat, glob_feat], -1).view(-1, fr_num, self.cnn_fdim * 2)
+        
+        #batch x cameraNum, framenum, v_hdim
+        _, seq_features = self.v_net(cam_features).permute(1, 0, 2).contiguous().view(-1, self.v_hdim)
+        #batch x cameraNum x framenum, mlp_dim[-1] 
+        seq_features = self.mlp(seq_features)
+        #batch, cameraNum, framenum, 2
+        logits = self.softmax(self.linear(seq_features)).view(-1, self.camera_num, fr_num, self.out_dim)
+
+        return torch.log(logits)
+
 models_func = {         
 'ds': DSNet,
+'dsv2': DSNetv2,
 'baseline':Baseline,
 'baseline_seq':Baseline_seq,
 'baseline_spac':Baseline_spac
